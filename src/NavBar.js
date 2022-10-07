7b5a38da3fbe19fc1c77ca8c64b99b8141df2bbc
Yuni <yshuh0126@gmail.com>
2022-10-06 21:12:38 -0700

    added basic nav bar and enhanced routings

---
+++ src/NavBar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import "./NavBar.css";
+
+export default function ContactUs() {
+  return <nav className="nav">
+    <Link to="/" className="miracool">Miracool</Link>
+    <ul>
+      <CustomLink to="/">Home</CustomLink>
+      <CustomLink to="/aboutus">About Us</CustomLink>
+      <CustomLink to="/services">Services</CustomLink>
+      <CustomLink to="/contactus">Contact Us</CustomLink>
+    </ul>
+  </nav>
+}
+
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to)
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+
+  )
+}
